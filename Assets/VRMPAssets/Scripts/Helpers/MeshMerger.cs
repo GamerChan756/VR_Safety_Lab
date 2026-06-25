6702b45b8d895e120810088d14c24b3587308998
BenHaeck <bnhaeckler@gmail.com>
2026-06-25 15:47:15 -0400

    D:
    I've given up on having the script handle separate materials using sub meshes, so when using the mesh merge script just have separate mesh mergers for different materials
    
    Also the demo room scene has been updated to reflect this new best practice

--- Assets/VRMPAssets/Scripts/Helpers/MeshMerger.cs
+++ Assets/VRMPAssets/Scripts/Helpers/MeshMerger.cs
@@ -1,37 +1,34 @@
 using System;
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Rendering;
 
 public class MeshMerger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        var meshFilter = GetComponent<MeshFilter> ();
-        var meshRenderer = GetComponent<MeshRenderer> ();
         var childMeshes = GetComponentsInChildren<MeshFilter> ();
-        var combinedInstances = new CombineInstance[childMeshes.Length - 1];
-        int instanceIndex = 0;
-        for (int i = 1; i < combinedInstances.Length; i++) {
-            if (childMeshes[i].gameObject == gameObject) continue;
-            combinedInstances[instanceIndex] = new CombineInstance ();
-            combinedInstances[instanceIndex].transform =
-                transform.worldToLocalMatrix *
-                childMeshes[i].transform.localToWorldMatrix;
-            combinedInstances[instanceIndex].mesh = childMeshes[i].mesh;
-            var renderer = childMeshes[i-instanceIndex].GetComponent<MeshRenderer> ();
-            for (int j = 0; j < meshRenderer.materials.Length; j++) {
-                if (renderer.materials[0].ComputeCRC() == meshRenderer.materials[j].ComputeCRC()) {
-                    combinedInstances[i-instanceIndex].subMeshIndex = j;
-                    Debug.Log ($"{instanceIndex} {j}");
-                    break;
-                }
-            }
-            renderer.enabled = false;
-            instanceIndex++;
+        var meshRenderer = GetComponent<MeshRenderer> ();
+        meshRenderer.materials = childMeshes[0].GetComponent<MeshRenderer> ().materials;
+        var instances = new CombineInstance[childMeshes.Length];
+
+        for (int i = 0; i < childMeshes.Length; i++) {
+            var combInstance = new CombineInstance () {
+                mesh = childMeshes[i].mesh,
+                transform = transform.worldToLocalMatrix * childMeshes[i].transform.localToWorldMatrix,
+                subMeshIndex = 0
+            };
+            instances[i] = combInstance;
+
+            Destroy(childMeshes[i].GetComponent<MeshRenderer> ());
+            
         }
         var mesh = new Mesh ();
-        mesh.CombineMeshes (combinedInstances, true);
+        mesh.CombineMeshes (instances);
+        var meshFilter = gameObject.AddComponent<MeshFilter> ();
         meshFilter.mesh = mesh;
+
     }
 
     // Update is called once per frame
